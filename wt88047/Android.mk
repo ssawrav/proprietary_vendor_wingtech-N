1e061da1bf4375935e71636513e185a44089f75f
Prema Chand Alugu <premaca@gmail.com>
2016-09-30 02:56:54 +0530

    Do not build ims

--- wt88047/Android.mk
+++ wt88047/Android.mk
@@ -74,16 +74,6 @@ LOCAL_PROPRIETARY_MODULE := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := ims
-LOCAL_MODULE_OWNER := wingtech
-LOCAL_SRC_FILES := proprietary/app/ims/ims.apk
-LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := APPS
-LOCAL_MODULE_SUFFIX := .apk
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := shutdownlistener
 LOCAL_MODULE_OWNER := wingtech
 LOCAL_SRC_FILES := proprietary/app/shutdownlistener/shutdownlistener.apk
--- wt88047/wt88047-vendor.mk
+++ wt88047/wt88047-vendor.mk
@@ -283,7 +283,6 @@ PRODUCT_PACKAGES += \
     libmm-abl \
     libqct_resampler \
     libtime_genoff \
-    ims \
     shutdownlistener \
     TimeService \
     CNEService \
