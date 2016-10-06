11441f0ffbc16df0b0e7365527ab224d10d70e04
Prema Chand Alugu <premaca@gmail.com>
2016-10-06 10:32:15 +0530

    remove mpdecision
    There is a nagging core-0 offline issue with mpdecision for our device. Get rid
    of it, and device should make sure other hotplug is enabled by default.

--- wt88047/wt88047-vendor.mk
+++ wt88047/wt88047-vendor.mk
@@ -36,7 +36,6 @@ PRODUCT_COPY_FILES += \
     vendor/wingtech/wt88047/proprietary/bin/loc_launcher:system/bin/loc_launcher \
     vendor/wingtech/wt88047/proprietary/bin/mm-pp-daemon:system/bin/mm-pp-daemon \
     vendor/wingtech/wt88047/proprietary/bin/mm-qcamera-daemon:system/bin/mm-qcamera-daemon \
-    vendor/wingtech/wt88047/proprietary/bin/mpdecision:system/bin/mpdecision \
     vendor/wingtech/wt88047/proprietary/bin/netmgrd:system/bin/netmgrd \
     vendor/wingtech/wt88047/proprietary/bin/qmuxd:system/bin/qmuxd \
     vendor/wingtech/wt88047/proprietary/bin/qseecomd:system/bin/qseecomd \
